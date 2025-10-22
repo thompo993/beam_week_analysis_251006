cc47006d727f498b497af7186305b82e6da65374
limalliolio <lisa.malliolio@gmail.com>
2025-10-22 12:01:16 +0100

    added peak finding and prepared data to be analysed.

--- beam/beam_source_comparison.py
+++ beam/beam_source_comparison.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import os
 from scipy.optimize import curve_fit
 import sys
+from scipy.signal import find_peaks
 
 FOLDER_PATH = r"C:\Users\xph93786\Desktop\first_plots"#folderpath goes here
 
 
 SAVE_PNG = False
 LOG = 'linear' #options: 'log', 'linear'
-channel = [1]
+channel = 1
 
 xlim = [None, 3000]
 ylim = [0, None]
@@ -20,12 +21,17 @@ size = [10,8]
 plt.figure(figsize=size)
 for file in os.listdir(FOLDER_PATH):
     file_path = os.path.join(FOLDER_PATH, file)
-    if "amplitude" in file:
-        df = pd.read_csv(file_path, on_bad_lines='skip', na_values=['No data', 'NaN', '', ' ', '  ', "m", "4magnesium"], skipinitialspace=True)
-    else:
-        df = pd.read_csv(file_path, on_bad_lines='skip', na_values=['No data', 'NaN', '', ' ', '  ', "m", "4magnesium"], skipinitialspace=True,usecols= channel)
+    df = np.loadtxt(file_path, delimiter=",", dtype=float)
+    if df.ndim > 1: 
+        df = df[:,channel]
+        df.flatten()
     x = range(len(df))
     norm = np.sum(df)
+
+    peaks, _ = find_peaks(df)
+    if len(peaks) != 0:
+        plt.scatter(peaks, df[peaks]/norm)
+
     plt.plot(x, df/norm, label = file)
 
 plt.title("CH %s" %channel)
